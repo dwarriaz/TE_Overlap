8f4d2ae5c69a87954087d27fe772f6afd891402b
dwarriaz <danielarriaza@eduroam-169-233-159-31.ucsc.edu>
2022-10-14 11:12:42 -0700

    Just commiting to commit, not sure what I did

--- LTR7_investigation/novel_transcript_TE.sh
+++ LTR7_investigation/novel_transcript_TE.sh
@@ -1,11 +1,10 @@
 #!/bin/bash  
 
-while read chrom rangeStart rangeEnd; do 
+while IFS=, read -r chrom rangeStart rangeEnd; do 
 
-chrom="\"$(echo $chrom)\""
-
-    mysql --batch --user=genome --host=genome-mysql.cse.ucsc.edu -N -A -D hg38 -e \
+    chrom="'$chrom'"
     
+    mysql --batch --user=genome --host=genome-mysql.cse.ucsc.edu -N -A -D hg38 -e \
     'select *
         from rmsk
         where genoName = '$chrom' and genoStart between '$rangeStart' and '$rangeEnd';'
